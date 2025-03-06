f81f228486ff865ad0572b2b71b43b978550b2af
voxo22 <62172895+voxo22@users.noreply.github.com>
2025-03-06 14:44:43 +0100

    Add files via upload
    v2.21

--- src/hackrf-sweep/src-java/jspectrumanalyzer/capture/ScreenCaptureH264.java
+++ src/hackrf-sweep/src-java/jspectrumanalyzer/capture/ScreenCaptureH264.java
@@ -4,44 +4,40 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.util.concurrent.TimeUnit;
+
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
 
 /**
- * Class to capture a video of the whole JFrame into the animated GIF, while capturing only when the view updates with the new data
+ * Class to capture a video of the whole JFrame into the H264 video, while capturing only when the view updates with the new data
  */
-public class ScreenCapture {
+public class ScreenCaptureH264 {
 	private final JFrame frame;
 	private final int width, height;
-	private final ImageOutputStream output;
-	private final GifSequenceWriter gif;
-	private final long captureMillis;
-	private final File outputFile;
+	private long startCaptureNano = 0;
 	private long startCaptureTS	= 0;
-	private long startedCapture	= 0;
 	private long lastFrameTS	= 0;
 	private int framesCaptured	= 0;
 	private int fheight = 0;
 	private ExecutorService saveThread	= Executors.newSingleThreadExecutor();
 	private long frameIIntervalMS;
-	private boolean rec;
+	private IMediaWriter writer;
 	
-	public ScreenCapture(JFrame frame, int initSecs, int captureSecs, int fps, int width, int height, String area, File outputFile) throws FileNotFoundException, IOException {
-		this.captureMillis	= captureSecs*1000L;
+	public ScreenCaptureH264(JFrame frame, int initSecs, int captureSecs, float fps, int width, int height, String area, String outputFile) throws FileNotFoundException, IOException {
 		this.frame	= frame;
 		this.width	= width;
 		this.height	= height;
-		this.outputFile	= outputFile;
 
-		this.startCaptureTS	= System.currentTimeMillis()+initSecs*1000L;
-		frameIIntervalMS = 1000/fps;
+		this.startCaptureNano	= System.nanoTime();
+		frameIIntervalMS = (long) (1000/fps);
 		if(area.equals("SPEC")) //record video of spectrum
 		{
 			switch(height) {
@@ -61,31 +57,25 @@ public class ScreenCapture {
 		}
 		frame.setSize(width, height);
 		
-		outputFile.delete();
-		
-	    output = new FileImageOutputStream(outputFile);
-	    gif = new GifSequenceWriter(output, BufferedImage.TYPE_3BYTE_BGR, (int)frameIIntervalMS, true);
+	    writer = ToolFactory.makeWriter(outputFile);
+	    writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, width, height);
 	}
 
 	public void captureFrame(boolean rec) {
-		this.rec = rec;
 		if (!SwingUtilities.isEventDispatchThread()) {
 			throw new IllegalStateException("Capture is NOT inside event dispatch thread!");
 		}
-		
+
 		long start	= System.currentTimeMillis();
 		
 		if (start-lastFrameTS < frameIIntervalMS || start < startCaptureTS) {
 			return;
 		}
 		lastFrameTS	= start;
+
 		
 		if (framesCaptured == -1)
 			return;
-		if (framesCaptured == 0) {
-			startedCapture	= System.currentTimeMillis();
-			System.out.println("Capture started...");
-		}
 
 		if (!rec) { /*System.currentTimeMillis() - startedCapture >= captureMillis*/ 
 			System.out.println("Capture finished... frames captured: "+framesCaptured);
@@ -93,15 +83,10 @@ public class ScreenCapture {
 //			task.cancel();
 			saveThread.submit(() -> {
 			try {
-				gif.close();
-			} catch (IOException e) {
+				writer.close();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}		
-			try {
-				output.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 			});
 			//System.exit(0);
 			return;
@@ -118,17 +103,17 @@ public class ScreenCapture {
 		g.dispose();
 
 		/**
-		 * convert to gif in a separate thread to not slow down swing's event thread
+		 * convert to video in a separate thread to not slow down swing's event thread
 		 */
 		saveThread.submit(() -> {
 			try {
-				gif.writeToSequence(capImage);
-			} catch (IOException e) {
+				writer.encodeVideo(0, capImage, System.nanoTime() - startCaptureNano, TimeUnit.NANOSECONDS);
+				//gif.writeToSequence(capImage);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
 		
-		//System.out.println("time to save gif "+(System.currentTimeMillis()-start));
 		//System.out.println("Frame: "+framesCaptured);
 	}
 }
